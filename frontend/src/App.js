ddf08652ee79b22a1a9da4e6590b514b6713e26b
r2d2 <aver.serg1870@gmail.com>
2021-09-02 12:31:08 +0300

    lesson_3 tasks added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from "react";
+
 import UserList from "./components/User";
 import axios from "axios";
 
--- todo/settings.py
+++ todo/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     # custom apps
     'usersapp',
+    'projectsapp',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,12 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'usersapp.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
